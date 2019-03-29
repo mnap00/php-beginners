1bb994fa4650d5a66417422baaa9f8cfbe64f75c
mnap00 <learn@10g.pl>
2019-03-30 00:47:01 +0100

    Add math file

---
+++ demo/11-math.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+
+<?php
+// phpcs:disable
+echo 30 + 12;
+echo '<br>';
+echo 55 - 21;
+echo '<br>';
+echo 24 * 23;
+echo '<br>';
+echo 87 / 42;
+echo '<br>';
+echo '<br>';
+
+echo 23 + 23 * 3 / 78;
+echo '<br>';
+echo 4 + 3 * 10;
+echo '<br>';
+echo (4 + 3) * 10;
+echo '<br>';
+echo '<br>';
+
+$number1 = 12;
+$number2 = 23;
+echo $number1 * $number2;
+
+?>
+
+</body>
+</html>
