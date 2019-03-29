25a6315b0dd7acfe856895ca9fd7f57259a0640b
mnap00 <learn@10g.pl>
2019-03-30 00:32:29 +0100

    Add practice section 1

--- app/1.php
+++ app/1.php
@@ -1,37 +1,33 @@
-<?php require "functions.php"; ?>
+<?php require "functions.php"; /* phpcs:disable */ ?>
 <?php require "includes/header.php";?>
 
-    <section class="content">
+	<section class="content">
 
-    <aside class="col-xs-4">
+		<aside class="col-xs-4">
 
-    <?php Navigation();?>
-            
-            
-    </aside><!--SIDEBAR-->
+			<?php Navigation();?>
 
+		</aside><!--SIDEBAR-->
 
-<article class="main-content col-xs-8">
-        
+		<article class="main-content col-xs-8">
 
+		<?php
 
-    <?php  
+		/**
+		 * Step 1: Use the echo function to say hello with html h1
+		 * tags embedded inside php.
+		 */
 
+			// This function prints 'Hello!' with HTML h1 tag.
+			echo '<h1>Hello!</h1>';
 
-    /* 
-           Step 1:  Use the Echo Function to say hello with html h1                  tags embedded inside php.
+		/**
+		 * Step 2: Write a comment above the echo function and explain
+		 * what that function is doing.
+		 */
 
+		?>
 
-           Step 2: Write a comment above the echo function and explain
-           what that function is doing.
-
-
-           */
-
-    ?>
-
-    
-
-        </article><!--MAIN CONTENT-->
+		</article><!--MAIN CONTENT-->
 
 <?php require "includes/footer.php"; ?>
