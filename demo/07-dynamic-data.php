fece82344d87a4898315fcd114d2a1be4fa9423c
mnap00 <learn@10g.pl>
2019-03-29 21:48:54 +0100

    Add dynamic text file

---
+++ demo/07-dynamic-data.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+
+<?php
+// phpcs:disable
+$title = 'Hello World';
+?>
+
+	<h1><?php echo $title; ?></h1>
+
+</body>
+</html>
