41067086ad96a148d4fe67dcaa8df2de7d22f9d3
mnap00 <learn@10g.pl>
2019-03-30 00:40:41 +0100

    Add variables file

---
+++ demo/10-variables.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+
+<?php
+// phpcs:disable
+$name = 'Dinsdale';
+$number = 100;
+$floating_point_number = 100.5; // needs more memory to process
+$markup = '<br>';
+
+echo $name;
+
+echo $markup;
+
+// string concatenation
+echo $name . ' ' . $number;
+
+?>
+
+</body>
+</html>
