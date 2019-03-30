f90f5c21ef601653e9bc79f82d311e41f3cd0dc2
mnap00 <learn@10g.pl>
2019-03-30 13:44:39 +0100

    Update practice section 3

--- app/3.php
+++ app/3.php
@@ -3,36 +3,65 @@
 
 	<section class="content">
 
-	<aside class="col-xs-4">
+		<aside class="col-xs-4">
 
-	<?php Navigation();?>
-			
-	</aside><!--SIDEBAR-->
+			<?php Navigation();?>
 
+		</aside><!--SIDEBAR-->
 
-<article class="main-content col-xs-8">
+		<article class="main-content col-xs-8">
 
-<?php
+		<?php
 
-/*  Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
+		/**
+		 * Step 1: Make an if statement with elseif and else
+		 * to finally display string saying, I love PHP.
+		 */
+		$lang = 'PHP';
+		if ($lang == 'JavaScript') {
+			echo 'I love JavaScript';
+		} elseif ($lang = 'PHP') {
+			echo 'I love PHP';
+		} else {
+			echo 'I don\'t know that language';
+		}
+		echo '<br>';
 
+		/**
+		 * Step 2: Make a for loop that displays 10 numbers.
+		 */
+		for ($i = 1; $i <= 10; $i++) {
+			echo $i . '<br>';
+		}
 
+		/**
+		 * Step 3: Make a switch statement that test againts
+		 * one condition with 5 cases.
+		 */
+		$grass = 'green';
 
-	Step 2: Make a forloop  that displays 10 numbers
+		switch ($grass) {
+			case 'yellow':
+				echo 'the grass is yellow';
+				break;
+			case 'green':
+				echo 'the grass is green';
+				break;
+			case 'white':
+				echo 'the grass is white';
+				break;
+			case 'black':
+				echo 'the grass is black';
+				break;
+			case 'red':
+				echo 'the grass is red';
+				break;
+			default:
+				echo 'there is no grass';
+		}
 
+		?>
 
-	Step 3 : Make a switch Statement that test againts one condition with 5 cases
+		</article><!--MAIN CONTENT-->
 
- */
-
-	
-?>
-
-
-
-
-
-
-</article><!--MAIN CONTENT-->
-	
 <?php require "includes/footer.php"; ?>
