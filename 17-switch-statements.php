ecbd873433417fbe59079e654f30f1690fec6ea0
mnap00 <learn@10g.pl>
2019-03-30 12:39:29 +0100

    Add switch statement file

---
+++ 17-switch-statements.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+
+<?php
+
+$number = 240;
+
+switch ($number) {
+	case 34:
+		echo 'it is 34';
+		break;
+	case 37:
+		echo 'it is 37';
+		break;
+	case 24:
+		echo 'it is 24';
+		break;
+	case 18:
+		echo 'it is 18';
+		break;
+	default:
+		echo 'we could not find anything';
+}
+
+?>
+
+</body>
+</html>
