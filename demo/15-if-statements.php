15c3e267f943642f69bc1aa0043db196993b02ef
mnap00 <learn@10g.pl>
2019-03-30 12:05:20 +0100

    Add if statement file

---
+++ demo/15-if-statements.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+
+<?php
+
+// If...else... statement.
+if (3 > 10) {
+	echo "three is less than ten";
+} elseif (4 > 5) {
+	echo "of course four is less than five";
+} else {
+	echo "it is not";
+}
+
+?>
+
+</body>
+</html>
