3f6f7d87c20e4ac57e7d279f442e53028405b88e
mnap00 <learn@10g.pl>
2019-03-30 08:31:19 +0100

    Add associative arrays file

---
+++ demo/13-associative-arrays.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+
+<?php
+// phpcs:disable
+$numbers = array( 10, 20, 94 );
+//echo $numbers[2] . '<br>';
+print_r( $numbers );
+echo '<br>';
+
+$names = array(
+    'first_name' => 'Dinsdale',
+    'last_name'  => 'Piranha',
+);
+print_r( $names );
+echo '<br>';
+echo $names['first_name'] . ' ' . $names[ 'last_name' ];
+
+?>
+
+</body>
+</html>
