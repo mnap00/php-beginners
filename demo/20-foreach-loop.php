178724ca4141dede6395f46e9deb580ea6780b45
mnap00 <learn@10g.pl>
2019-03-30 13:10:36 +0100

    Add foreach loop file

---
+++ demo/20-foreach-loop.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>20. Foreach Loop</title>
+</head>
+<body>
+
+<?php
+
+$numbers = array(243, 345, 187, 347, 2083, 35235, 51553);
+
+foreach ($numbers as $number) {
+	echo $number . '<br>';
+}
+
+?>
+
+</body>
+</html>
