36db2705841c835c992476efd4b536c901e792f6
mnap00 <learn@10g.pl>
2019-03-30 12:21:40 +0100

    Add comparison and logical operators file

---
+++ demo/16-comparison-operators.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+
+<h2>Comparison Operators</h2>
+<pre>
+	equal         ==
+	identical     ===
+	compare       > < >= <= <>
+	not equal     !=
+	not identical !==
+</pre>
+
+<h2>Logical Operators</h2>
+<pre>
+	and    &&
+	or     ||
+	not    !
+</pre>
+
+<br>
+
+<?php
+
+if (4 === '4' || 5 < 10) {
+	echo "it is true" . '<br>';
+}
+
+if (4 === 4 && 5 < 10) {
+	echo "it is too";
+}
+
+?>
+
+</body>
+</html>
