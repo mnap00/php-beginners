b3e0eeb9c1bf7ca0b094e8f123277e24b5e936c9
mnap00 <learn@10g.pl>
2019-03-30 08:25:35 +0100

    Add arrays file

---
+++ demo/12-arrays.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+
+<?php
+// phpcs:disable
+$number_list = array( 23, 5675, '2345', 'string', '<h1>Hello</h1>' );
+$altarr_number_list = [];
+
+//print_r($number_list);
+
+echo $number_list[1];
+//echo '<br>';
+
+?>
+
+</body>
+</html>
