2bea5fc9333c61dd87b81f72beed68ee7a8cc589
mnap00 <learn@10g.pl>
2019-03-31 20:55:50 +0200

    Add string functions file

---
+++ demo/29-string-functions.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>29. String Functions</title>
+</head>
+<body>
+
+<?php
+
+$string = 'Hello World! How are you?';
+
+echo strlen($string); // length of a string
+echo '<br>';
+
+echo strtoupper($string);
+echo '<br>';
+
+echo strtolower($string);
+echo '<br>';
+
+?>
+
+</body>
+</html>
