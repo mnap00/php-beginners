d4ccc6ae9597b28173562807c2166fc46f23a656
mnap00 <learn@10g.pl>
2019-03-31 19:46:03 +0200

    Add constants file

---
+++ demo/26-constants.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>26. Constants</title>
+</head>
+<body>
+
+<?php
+
+$number = 10;
+echo $number . '<br>';
+
+define('NAME', 1000);
+echo NAME;
+
+?>
+
+</body>
+</html>
