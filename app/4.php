2edaabf3e32099561047242243d22ac55a321184
mnap00 <learn@10g.pl>
2019-03-31 20:12:01 +0200

    Add practice section 4

--- app/4.php
+++ app/4.php
@@ -3,34 +3,46 @@
 
 	<section class="content">
 
-	<aside class="col-xs-4">
+		<aside class="col-xs-4">
 
-	<?php Navigation();?>
-			
-		
-	</aside><!--SIDEBAR-->
+			<?php Navigation();?>
 
+		</aside><!--SIDEBAR-->
 
-<article class="main-content col-xs-8">
+		<article class="main-content col-xs-8">
 
-	
-	<?php
+		<?php
 
-	/*  Step1: Define a function and make it return a calculation of 2 numbers
+		/**
+		 * Step1: Define a function and make it return a calculation
+		 * of 2 numbers.
+		 */
+		function multiplyNumbers() {
+			$number1 = 23;
+			$number2 = 78;
+			$multiply = $number1 * $number2;
+			return $multiply;
+		}
 
-	Step 2: Make a function that passes parameters and call it using parameter values
+		$calculation = multiplyNumbers();
+		echo $calculation;
 
+		echo '<br>';
 
-	*/
+		/**
+		 * Step 2: Make a function that passes parameters and call it
+		 * using parameter values.
+		 */
+		function greetSomeone($greet, $who) {
+			$say_hello = $greet . ' ' . $who;
+			return $say_hello;
+		}
 
-	
-	?>
+		$greetings = greetSomeone('Hello', 'World');
+		echo $greetings;
 
+		?>
 
-
-
-
-</article><!--MAIN CONTENT-->
-
+		</article><!--MAIN CONTENT-->
 
 <?php require "includes/footer.php"; ?>
