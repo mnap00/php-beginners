72d0c69de21a19b1f9321c7a4f6d8416999cdc4d
mnap00 <learn@10g.pl>
2019-03-31 20:44:34 +0200

    Add math functions file

---
+++ demo/28-math-functions.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>28. Math Functions</title>
+</head>
+<body>
+
+<?php
+
+// Example PHP built in math functions.
+
+echo pow(2, 7);
+echo '<br>';
+
+echo rand(1, 1000);
+echo '<br>';
+
+echo sqrt(100);
+echo '<br>';
+
+echo ceil(4.6);
+echo '<br>';
+
+echo floor(4.6);
+echo '<br>';
+
+echo round(4.6);
+echo '<br>';
+
+?>
+
+</body>
+</html>
