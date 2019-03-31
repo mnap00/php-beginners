4db698421c838d15621b9ca0e9de647cf9f4f557
mnap00 <learn@10g.pl>
2019-03-31 21:02:17 +0200

    Add array functions file

---
+++ demo/30-array-functions.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>30. Array Functions</title>
+</head>
+<body>
+
+<?php
+
+$list = [345, 453, 890, 390, 582, 297, 278];
+
+echo max($list);
+echo '<br>';
+
+echo min($list);
+echo '<br>';
+
+sort($list); // sort array
+print_r($list); // print all values to screen
+echo '<br>';
+
+?>
+
+</body>
+</html>
