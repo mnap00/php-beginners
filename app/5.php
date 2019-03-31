7bbeed4a84ca2feb3b1b29400bf808427a0f2956
mnap00 <learn@10g.pl>
2019-03-31 21:27:30 +0200

    Add practice section 5

--- app/5.php
+++ app/5.php
@@ -1,36 +1,41 @@
 <?php require "functions.php"; ?>
 <?php require "includes/header.php";?>
+
 	<section class="content">
 
 		<aside class="col-xs-4">
-	<?php Navigation();?>
-			
-			
+
+			<?php Navigation();?>
+
 		</aside><!--SIDEBAR-->
 
+		<article class="main-content col-xs-8">
 
-<article class="main-content col-xs-8">
-
-	
-	<?php
+		<?php
 
 
-	/*  Step1: Use a pre-built math function here and echo it
+		/**
+		 * Step1: Use a pre-built math function here and echo it.
+		 */
+		echo decbin(42);
+		echo '<br>';
 
+		/**
+		 * Step 2: Use a pre-built string function here and echo it.
+		 */
+		$string = 'Hello World';
+		echo str_rot13($string);
+		echo '<br>';
 
-	Step 2:  Use a pre-built string function here and echo it
+		/**
+		 * Step 3: Use a pre-built Array function here and echo it.
+		 */
+		$values = [345, 362, 9789, 4537, 66224];
+		echo array_sum($values);
+		echo '<br>';
 
+		?>
 
-	Step 3:  Use a pre-built Array function here and echo it
+		</article><!--MAIN CONTENT-->
 
-	*/
-
-	
-	?>
-
-
-
-
-
-</article><!--MAIN CONTENT-->
 <?php require "includes/footer.php"; ?>
