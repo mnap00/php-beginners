51339605fb0489bc4478cfa31c0334894d53305a
mnap00 <learn@10g.pl>
2019-04-01 21:36:18 +0200

    Add practice section 6

--- app/6.php
+++ app/6.php
@@ -1,29 +1,34 @@
-
 <?php require "functions.php"; ?>
 <?php require "includes/header.php";?>
 
 	<section class="content">
 
 		<aside class="col-xs-4">
-		
-	<?php Navigation();?>
-			
+
+			<?php Navigation();?>
+
 		</aside><!--SIDEBAR-->
 
+		<article class="main-content col-xs-8">
 
-<article class="main-content col-xs-8">
- 
+			<form action="6.php" method="post">
+				<input id="search" type="search" name="search">
+				<input type="submit" value="Search" name="submit">
+			</form>
 
-	<?php
+		<?php
 
-	/*  Step1: Make a form that submits one value to POST super global
+		/**
+		 * Step1: Make a form that submits one value to POST super global
+		 */
+		if (isset($_POST['submit'])) {
+			$string = $_POST['search'];
 
+			echo 'Database was searched for ' . $string . '.';
+		}
 
-	*/
+		?>
 
-	
-	?>
+		</article><!--MAIN CONTENT-->
 
-
-</article><!--MAIN CONTENT-->
 <?php require "includes/footer.php"; ?>
