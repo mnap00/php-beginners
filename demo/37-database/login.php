81f9849bdfce5558c3ee047ff5e6203ac6e10a0c
mnap00 <learn@10g.pl>
2019-04-01 23:41:46 +0200

    Add form markup (use bootstrap)

---
+++ demo/37-database/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login Page</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+
+	<div class="container">
+		<div class="col-xs-6">
+			<form action="login.php" method="post">
+				<div class="form-group">
+					<label for="username">Username</label>
+					<input class="form-control" type="text" name="username">
+				</div>
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input class="form-control" type="password" name="password">
+				</div>
+				<input class="btn btn-primary" type="submit" value="Submit" name="submit">
+			</form>
+		</div>
+	</div>
+
+</body>
+</html>
