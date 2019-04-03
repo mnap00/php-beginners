be9c11251722a2f9e4857337e0951a8603945ffa
mnap00 <learn@10g.pl>
2019-04-03 22:29:06 +0200

    Add (C) create record into the database file

---
+++ demo/37-database/44-create-record.php
@@ -0,0 +1,52 @@
+<?php
+
+if (isset($_POST['submit'])) {
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	$connection = mysqli_connect('localhost', 'root', '', 'loginapp');
+
+	if ($connection) {
+		echo 'We are connected.';
+	} else {
+		die('Database connection failed.');
+	}
+
+	$query = 'INSERT INTO users(username, password) ';
+	$query .= 'VALUES (\'' . $username . '\', \'' . $password . '\')';
+	$result = mysqli_query($connection, $query);
+
+	if (!$result) {
+		die('Query FAILED' . mysqli_error());
+	}
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>44. (C) Create Record into the Database</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+
+	<div class="container">
+		<div class="col-xs-6">
+			<form action="44-create-record.php" method="post">
+				<div class="form-group">
+					<label for="username">Username</label>
+					<input class="form-control" type="text" name="username">
+				</div>
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input class="form-control" type="password" name="password">
+				</div>
+				<input class="btn btn-primary" type="submit" value="Submit" name="submit">
+			</form>
+		</div>
+	</div>
+
+</body>
+</html>
