779aa1f323cd121440e4fc8c3bd1b8c7462bc253
mnap00 <learn@10g.pl>
2019-04-04 23:21:44 +0200

    Refactor the update query into a function

--- demo/37-database/functions.php
+++ demo/37-database/functions.php
@@ -3,6 +3,7 @@
 
 function show_all_data() {
 	global $connection;
+
 	$query = 'SELECT * FROM users';
 	$result = mysqli_query($connection, $query);
 
@@ -10,9 +11,27 @@ function show_all_data() {
 		die('Query FAILED' . mysqli_error());
 	}
 
-
 	while ($row = mysqli_fetch_assoc($result)) {
 		$id = $row['id'];
 		echo '<option value="' . $id . '">' . $id . '</option>';
+	}
+}
+
+function update_table() {
+	global $connection;
+
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$id = $_POST['id'];
+
+	$query = 'UPDATE users SET ';
+	$query .= 'username = \'' . $username . '\', ';
+	$query .= 'password = \'' . $password . '\' ';
+	$query .= 'WHERE id = ' . $id;
+
+	$result = mysqli_query($connection, $query);
+
+	if (!$result) {
+		die('QUERY FAILED: ' . mysqli_error($connection));
 	}
 }
