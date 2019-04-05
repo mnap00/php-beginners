8720a42085723e63d11b0c0cd8d4dc4c6f8b9358
mnap00 <learn@10g.pl>
2019-04-05 22:10:17 +0200

    Add password encryption example

--- demo/37-database/functions.php
+++ demo/37-database/functions.php
@@ -10,6 +10,12 @@ function create_rows() {
 		$username = mysqli_real_escape_string($connection, $username);
 		$password = mysqli_real_escape_string($connection, $password);
 
+		$hash_format = '$2y$10$';
+		$salt = 'iusesomecrazystrings22';
+		$hash_f_and_salt = $hash_format . $salt;
+
+		$password = crypt($password, $hash_f_and_salt);
+
 		$query = 'INSERT INTO users(username, password) ';
 		$query .= 'VALUES (\'' . $username . '\', \'' . $password . '\')';
 
