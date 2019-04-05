cf8fbea680bbd37711f99bc5b5518fd0987b19b1
mnap00 <learn@10g.pl>
2019-04-05 21:21:20 +0200

    Add practice section 7

--- app/7.php
+++ app/7.php
@@ -1,39 +1,50 @@
 <?php require "functions.php"; ?>
 <?php require "includes/header.php";?>
-	
 
 	<section class="content">
 
 		<aside class="col-xs-4">
 
-	<?php Navigation();?>
-			
-			
+			<?php Navigation();?>
+
 		</aside><!--SIDEBAR-->
 
 
-	<article class="main-content col-xs-8">
-	
-	
-	
-	<?php
+		<article class="main-content col-xs-8">
 
-	/*  Step 1 - Create a database in PHPmyadmin
+		<?php
+		/**
+		 * Step 1 - Create a database in PHPmyadmin.
+		 *
+		 * Step 2 - Create a table like the one from the lecture.
+		 *
+		 * Step 3 - Insert some Data.
+		 *
+		 * Step 4 - Connect to Database and read data.
+		 */
+		$link = mysqli_connect('localhost', 'root', '', '7app');
 
-	Step 2 - Create a table like the one from the lecture
+		if (!$link) {
+			die('Database connection failed!');
+		}
 
-	Step 3 - Insert some Data
+		$query = 'SELECT * FROM users';
 
-	Step 4 - Connect to Database and read data
+		$result = mysqli_query($link, $query);
 
-	*/
-	
-	?>
+		if (!$result) {
+			die('Query failed!' . '<br>' . mysqli_error($link));
+		}
 
+		while ($row = mysqli_fetch_assoc($result)) {
+			print $row['username'];
+			print '<pre>';
+			print_r($row);
+			print '</pre>';
+		}
 
+		?>
 
-
-
-</article><!--MAIN CONTENT-->
+		</article><!--MAIN CONTENT-->
 
 <?php require "includes/footer.php"; ?>
