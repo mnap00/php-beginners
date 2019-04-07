877e4a8854044edae3876febe096d67b2d138fdd
mnap00 <learn@10g.pl>
2019-04-07 21:49:24 +0200

    Add using sessions files

---
+++ demo/64-session1.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+$_SESSION['greeting'] = 'Hello World!';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>64. How Use Sessions (1)</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ demo/64-session2.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+echo $_SESSION['greeting'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>64. How Use Sessions (2)</title>
+</head>
+<body>
+
+</body>
+</html>
