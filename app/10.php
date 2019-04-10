d8b2a8809b554936757f20e835d57e1f6a2fd8d0
mnap00 <learn@10g.pl>
2019-04-10 23:43:52 +0200

    Add practice section 10

--- app/10.php
+++ app/10.php
@@ -1,40 +1,49 @@
 <?php require "functions.php"; ?>
 <?php require "includes/header.php";?>
+
 <section class="content">
 
 	<aside class="col-xs-4">
 
-	<?php Navigation();?>
-			
-			
+		<?php Navigation();?>
+
 	</aside><!--SIDEBAR-->
 
-<article class="main-content col-xs-8">
+	<article class="main-content col-xs-8">
 
-	
 	<?php
 
-	/*  Step 1: Use the Make a class called Dog
+	/**
+	 * Step 1: Use the Make a class called Dog.
+	 *
+	 * Step 2: Set some properties for Dog, Example,
+	 * eye colors, nose, or fur color.
+	 *
+	 * Step 4: Make a method named ShowAll that echos all the properties.
+	 *
+	 * Step 5: Instantiate the class / create object and call it pitbull.
+	 *
+	 * Step 6: Call the method ShowAll.
+	 */
+	class Dog {
 
-	Step 2: Set some properties for Dog, Example, eye colors, nose, or fur color
+		var $eye = 'brown';
+		var $nose = 'pink';
+		var $fur = 'brown';
 
-	Step 4: Make a method named ShowAll that echos all the properties
+		function showAll() {
+			echo 'Eye color is: ' . $this->eye . '<br>';
+			echo 'Nose color is: ' . $this->nose . '<br>';
+			echo 'Fur color is: ' . $this->fur . '<br>';
+		}
+	}
 
-	Step 5: Instantiate the class / create object and call it pitbull
+	$pitbull = new Dog;
 
-	Step 6: Call the method ShowAll
+	$pitbull->showAll();
 
-
-
-
-	*/
-	
 	?>
 
-
-
-
-
-</article><!--MAIN CONTENT-->
+	</article><!--MAIN CONTENT-->
 
 <?php require "includes/footer.php"; ?>
